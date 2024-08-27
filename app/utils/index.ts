672353957b38ed509ba8d9816b395c5955683744
vitalii-karietin-ew <vitalii.karietin@extrawest.com>
2024-08-27 16:03:26 +0300

    debug

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -5,7 +5,7 @@ import { PODCAST_INDEX_API_BASE_URL } from '@/app/constants';
 const podcastIndexApiKey: string = process.env.NEXT_PODCAST_INDEX_API_KEY as string;
 const podcastIndexApiKeySecret: string = process.env.NEXT_PODCAST_INDEX_API_KEY_SECRET as string;
 const apiHeaderTime = Math.floor(new Date().getTime()/1000.0);
-
+console.log(podcastIndexApiKey, podcastIndexApiKeySecret);
 export const podcastIndexAxiosInstance: AxiosInstance = axios.create({
   baseURL: PODCAST_INDEX_API_BASE_URL,
   timeout: 10000,
